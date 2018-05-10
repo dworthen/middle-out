fe1699f7ac44dcb94bb5b291ce1e054401e7d7c4
dworthen <dworthen@wsu.edu>
2018-05-10 07:59:39 -0700

    fix: Resolve compilation error with Required.ts
    Strongly type Required validator to support
    creating declarations during compilation.

--- src/validators/Required.ts
+++ src/validators/Required.ts
@@ -1,7 +1,8 @@
-import { registerValidator } from '../Validation';
+import { registerValidator, ValidatorConfig } from '../Validation';
 import { checkTargetAndProperty } from '../Utils';
 
-export const Required = registerValidator("Required", (target, property) => { 
+export const Required: (config?: ValidatorConfig | undefined) => (target: {[key:string]: any}, property: string) => void = 
+registerValidator("Required", (target, property) => { 
     checkTargetAndProperty(target, property);
 
     return target[property] !== null && target[property] !== undefined;
