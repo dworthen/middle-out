e13a95ea326f31e3ad1e43554a2d5fc0665b4b8a
dworthen <dworthen@wsu.edu>
2018-05-14 13:55:01 -0700

    fix: Expose range and instanceOf validators

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export { registerMetaData, getMetaData } from './Reflection';
 export { registerValidator, getValidators, validate, errorTemplate } from './Validation';
 export { checkTargetAndProperty } from './Utils';
-export { required, stringLength, typeOf } from './validators/index';
+export { 
+    required, 
+    stringLength, 
+    typeOf, 
+    instanceOf,
+    range 
+} from './validators/index';
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,3 +1,5 @@
 export { required } from './required';
 export { stringLength } from './stringLength';
 export { typeOf } from './typeOf';
+export { instanceOf } from './instanceOf';
+export { range } from './range';
